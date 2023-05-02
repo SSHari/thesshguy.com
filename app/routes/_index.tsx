e15b763e8a718f7b3b46db210d1cb7e7a34860eb
SSHari <thesshguy@gmail.com>
2023-05-02 01:43:17 -0400

    Add tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="w-full text-center">Welcome to Remix</h1>
       <ul>
         <li>
           <a
