10ccb649ea712176abfda64679cbea142561ac15
SSHari <thesshguy@gmail.com>
2023-05-06 12:24:13 -0400

    fix: 🐛 add a title attribute to IconLink components

--- app/components/IconLink.tsx
+++ app/components/IconLink.tsx
@@ -3,11 +3,12 @@ import { SVGProps } from 'react';
 type IconLinkProps = {
   Component: (props: SVGProps<SVGSVGElement>) => JSX.Element;
   link: string;
+  title: string;
 };
 
-export function IconLink({ Component, link }: IconLinkProps) {
+export function IconLink({ Component, link, title }: IconLinkProps) {
   return (
-    <a href={link}>
+    <a href={link} title={title}>
       <Component className="duration:500 h-8 w-8 fill-gray-400 transition hover:scale-125 hover:fill-gray-600 md:h-9 md:w-9" />
     </a>
   );
